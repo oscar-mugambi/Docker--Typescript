b0c2068d30b32619cc10dd32f2131fa454ef10d2
oscar-mugambi <oscarmugambi@gmail.com>
2022-03-30 23:37:02 +0300

    switched from tsc to ts-node-dev

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
